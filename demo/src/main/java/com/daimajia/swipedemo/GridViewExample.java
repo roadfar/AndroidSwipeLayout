9a449d916a58ce8f732bd5e04f716501c5c8bec7
Roadfar <839377654@qq.com>
2015-07-15 11:16:29 +0800

    测试本地提交
    添加hello github

--- demo/src/main/java/com/daimajia/swipedemo/GridViewExample.java
+++ demo/src/main/java/com/daimajia/swipedemo/GridViewExample.java
@@ -36,7 +36,7 @@ public class GridViewExample extends Activity{
             }
         });
 
-
+//hello github
         gridView.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
